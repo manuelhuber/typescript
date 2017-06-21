dc55c343227a4122afa81b9828a628eeb342091a
Manuel Huber <nelo112@web.de>
2017-06-21 10:09:17 +0200

    added util

--- util.js
+++ util.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Functions can be declared and exported/imported on their own
 function foobar() {
     console.log('foobar');
 }
 exports.foobar = foobar;
-function otherUtil() {
+function add(a, b) {
+    return a + b;
 }
-exports.otherUtil = otherUtil;
+exports.add = add;
--- util.ts
+++ util.ts
@@ -1,7 +1,8 @@
+// Functions can be declared and exported/imported on their own
 export function foobar() {
     console.log('foobar');
 }
 
-export function otherUtil() {
-
+export function add(a : number, b : number) : number {
+    return a + b;
 }
